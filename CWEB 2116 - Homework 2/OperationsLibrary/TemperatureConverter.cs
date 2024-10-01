e885544e90347aad47a8b07a7fe4c8ecbaeea987
spcasagrande <146010636+spcasagrande@users.noreply.github.com>
2024-09-30 20:21:59 -0500

    Add files via upload

---
+++ CWEB 2116 - Homework 2/OperationsLibrary/TemperatureConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationsLibrary
+{
+    public class TemperatureConverter
+    {
+		public static double CelsiusToFahrenheit(double celsius)
+		{
+			double fahrenheit = ((celsius * 1.8) + 32);
+			double result = Math.Round(fahrenheit, 2);
+			return result;
+		}
+		public static double FahrenheitToCelsius(double fahrenheit)
+		{
+			double celsius = (0.55555556 * (fahrenheit - 32));
+			double result = Math.Round(celsius, 2);
+			return result;
+		}
+	}
+}
