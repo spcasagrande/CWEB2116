1870b39406fb216416954df12d62871179bcb4d9
spcasagrande <146010636+spcasagrande@users.noreply.github.com>
2024-09-30 20:10:06 -0500

    Add files via upload

---
+++ CWEB 2116 - Homework 2/OperationsApplication/PerformTemperatureConversion.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationsApp
+{
+	class PerformTemperatureConversion
+	{
+		public void performTemperatureConversion()
+		{
+			bool tryagain = true;
+
+			while (tryagain)
+			{
+				Console.WriteLine("Do you wish to convert: ");
+				Console.WriteLine("1. Celsius to Fahrenheit");
+				Console.WriteLine("2. Fahrenheit to Celsius");
+				Console.WriteLine("3. Exit");
+				string answer = Console.ReadLine();
+
+				switch (answer)
+				{
+					case "1":
+						Console.WriteLine("\nEnter Celsius temperature to convert: ");
+						double celsius = double.Parse(Console.ReadLine());
+						double fahrenheitRes = OperationsLibrary.TemperatureConverter.CelsiusToFahrenheit(celsius);
+						Console.WriteLine("Temperature in Celsius = " + celsius);
+						Console.WriteLine("Temperature in Fahrenheit = " + fahrenheitRes + "\n");
+						break;
+					case "2":
+						Console.WriteLine("\nEnter Fahrenheit temperature to convert: ");
+						double fahrenheit = double.Parse(Console.ReadLine());
+						double celsiusRes = OperationsLibrary.TemperatureConverter.FahrenheitToCelsius(fahrenheit);
+						Console.WriteLine("Temperature in Fahrenheit = " + fahrenheit);
+						Console.WriteLine("Temperature in Celsius = " + celsiusRes + "\n");
+						break;
+					case "3":
+						tryagain = false;
+						break;
+					default:
+						Console.WriteLine("Invalid choice. Try again\n");
+						break;
+				}
+			}
+        }
+	}
+}
---
+++ CWEB 2116 - Homework 2/OperationsApplication/TemperatureApp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationsApp
+{
+	class TemperatureApp
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Temperature Conversion");
+			Console.WriteLine("----------------------");
+			PerformTemperatureConversion conversion1 = new PerformTemperatureConversion();
+			conversion1.performTemperatureConversion();
+		}
+	}
+}
